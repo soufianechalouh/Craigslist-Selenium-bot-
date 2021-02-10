a3248a5b2e671a108fb2d7ab3d198420746834cc
Soufiane CHALOUH <soufianechalouh@gmail.com>
2021-02-10 20:12:17 +0100

    new bot to post in electronic

---
+++ craigslist_electronic_posting_bot.py
@@ -0,0 +1,108 @@
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+import os.path
+
+
+def find(browser):
+    add_images = browser.find_element_by_xpath('//input[@name="file"]')
+    if add_images:
+        return add_images
+    else:
+        return False
+
+
+browser = webdriver.Chrome('./chromedriver')
+
+browser.get('https://post.craigslist.org/')
+
+# Select Location
+browser.find_element_by_class_name('ui-selectmenu-text').click()
+
+browser.find_element_by_xpath('//li[@id="ui-id-28"]').click()
+
+browser.find_element_by_xpath('//button[@name="go"]').click()
+
+
+# Post Type Page
+browser.find_element_by_xpath('//input[@name="id" and @value="fso"]').click()
+
+# Category
+browser.find_element_by_xpath('//input[@name="id" and @value="96"]').click()
+
+# Main post page
+title_area = browser.find_element_by_xpath('//input[@name="PostingTitle" and @id="PostingTitle"]')
+title_area.send_keys('Brand New Fire Stick Lite Fully Loaded Jailbroken With Latest Apps')
+
+city = browser.find_element_by_xpath('//input[@name="geographic_area" and @id="geographic_area"]')
+city.send_keys('Brooklyn')
+
+postal_code = browser.find_element_by_xpath('//input[@name="postal" and @id="postal_code"]')
+
+postal_code.send_keys(74324)
+time.sleep(1)
+
+# main text section
+main_text = browser.find_element_by_xpath('//textarea[@name="PostingBody" and @id="PostingBody"]')
+main_text.send_keys('Check out a video presentation of the property: https://youtu.be/Cnvft2EaY')
+
+# sale_manufacturer
+sale_manufacturer = browser.find_element_by_xpath('//input[@name="sale_manufacturer"]')
+sale_manufacturer.send_keys("Amazon")
+
+# Price
+price = browser.find_element_by_xpath('//input[@name="price"]')
+price.send_keys(54)
+
+#
+# laundry = browser.find_element_by_xpath('//span[@id="ui-id-2-button"]').click()
+# browser.find_element_by_xpath('//li[@id="ui-id-24"]').click()
+#
+# bedrooms = browser.find_element_by_xpath('//span[@id="Bedrooms-button"]').click()
+# browser.find_element_by_xpath('//li[@id="ui-id-28"]').click()
+#
+# bathrooms = browser.find_element_by_xpath('//span[@id="ui-id-4-button"]').click()
+# browser.find_element_by_xpath('//li[@id="ui-id-36"]').click()
+
+email = browser.find_element_by_xpath('//input[@name="FromEMail"]')
+email.send_keys('test@gmail.com')
+
+browser.find_element_by_xpath('//button[@name="go" and @value="continue"]').click()
+
+# Find Location
+street = browser.find_element_by_xpath('//input[@name="xstreet0" and @id="xstreet0"]')
+street.send_keys('4104 Shimmering Cv')
+city_location = browser.find_element_by_xpath('//input[@name="city" and @id="city"]')
+city_location.send_keys('Austin')
+
+find_location = browser.find_element_by_xpath('//button[@id="search_button"]').click()
+time.sleep(1)
+
+location_submit = browser.find_element_by_xpath('//button[@class="continue bigbutton"]').click()
+
+# Send Pictures
+browser.find_element_by_xpath('//a[@id="classic"]').click()
+
+add_images = browser.find_element_by_xpath('//input[@name="file"]')
+
+img = []
+path = './device_images'
+valid_image = ['.jpg', '.gif', '.png', '.tga']
+for f in os.listdir(path):
+    ext = os.path.splitext(f)[1]
+    if ext.lower() not in valid_image:
+        continue
+    print(f)
+    img.append(f'/random_house/{f}')
+
+for i in sorted(img):
+    if add_images != False:
+        print(os.getcwd() + i)
+        add_images.send_keys(os.getcwd() + i)
+        add_images = WebDriverWait(browser, 3).until(find)
+    else:
+        continue
+
+browser.find_element_by_xpath('//button[@value="Done with Images"]').click()
